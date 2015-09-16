a79a978a61636e758d3efca026c232f8ce1c6478
Ruben Undheim <ruben.undheim@gmail.com>
2015-09-16 19:24:40 +0200

    Fixed typo

--- debian/man/genmanpages.sh
+++ debian/man/genmanpages.sh
@@ -2,4 +2,4 @@
 
 
 txt2man -d "${CHANGELOG_DATE}" -t OSMO-ARFCN             -s 1 osmo-arfcn.txt           > osmo-arfcn.1
-txt2man -d "${CHANGELOG_DATE}" -t OSMO-AUC-GEN           -s 1 osmo-auc-gen.txt         > osmo-auc-gen.txt
+txt2man -d "${CHANGELOG_DATE}" -t OSMO-AUC-GEN           -s 1 osmo-auc-gen.txt         > osmo-auc-gen.1
