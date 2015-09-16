187858aa16330ab11cb543a091543725782f6d0d
Ruben Undheim <ruben.undheim@gmail.com>
2015-09-16 19:21:46 +0200

    Added empty man pages

---
+++ debian/man/genmanpages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+txt2man -d "${CHANGELOG_DATE}" -t OSMO-ARFCN             -s 1 osmo-arfcn.txt           > osmo-arfcn.1
+txt2man -d "${CHANGELOG_DATE}" -t OSMO-AUC-GEN           -s 1 osmo-auc-gen.txt         > osmo-auc-gen.txt
